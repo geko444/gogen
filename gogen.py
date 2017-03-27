bb92f201eac78d2b6dcbbbc74d407b4663f6c2e7
geko444 <geko123@gmail.com>
2017-03-27 20:05:04 +0100

    Gave up on graph stuff, gogen.py working, new inputs.

--- gogen.py
+++ gogen.py
@@ -72,13 +72,24 @@ def common_neighbours(pos_list):
 
 def possible_positions(l):
     if l in letters_set:
-        return positions[l]
+        return [positions[l]]
     else:
         pos_adj = []
         for l_adj in letters_adj[l]:
             if l_adj in letters_set:
                 pos_adj.append(positions[l_adj])
         return common_neighbours(pos_adj)
+
+def neighbours_of_multiple_positions(positions):
+    setlist = [neighbours(pos) for pos in positions]
+    return set.union(*setlist)
+
+def possible_positions_if_neighbours_unset(l):
+    pos_la = []
+    for la in letters_adj[l]:
+        if la in letters_unset and possible_positions(la) != None:
+            pos_la.append(neighbours_of_multiple_positions(possible_positions(la)))
+    return set.intersection(*pos_la)
 
 def all_possible():
     all_pos = {}
@@ -89,13 +100,19 @@ def all_possible():
         all_pos[letter] = possible_positions(letter)
     return all_pos
 
+def all_possible_remove_none():
+    all_pos = all_possible()
+    for l in letters_unset:
+        if all_pos[l] == None:
+            all_pos[l] = list(possible_positions_if_neighbours_unset(l))
+    return all_pos
 
 def set_unique(l):
     if l in letters_set:
         pass
     else:
         pos_pos = possible_positions(l)
-        if pos_pos == 0:
+        if pos_pos == None:
             pass
         elif len(pos_pos) == 1:
             place_letter(l, list(pos_pos)[0])
@@ -114,7 +131,7 @@ def possible_letters():
     for l in letters_unset:
         pos_pos = possible_positions(l)
 #        print l, pos_pos
-        if pos_pos != 0:
+        if pos_pos != None:
             for pos in pos_pos:
                 pos_let[pos].append(l)
     return pos_let
@@ -151,7 +168,63 @@ def set_cycle_fill_first():
     set_unique_cycle()
     fill_first_unique()
 
-get_input(1)
+def check_adjacent(a, b):
+    ai, aj = a % 5, a / 5
+    bi, bj = b % 5, b / 5
+    i_diff = abs(ai - bi)
+    j_diff = abs(aj - bj)
+    if max(i_diff, j_diff) == 1:
+        return True
+    else:
+        return False
 
-print positions
-print all_possible()
+def unset_adjacent_pair(l, m, pos_pos):
+    if m in letters_adj[l] and all(k in letters_unset for k in [l, m]):
+        l_pos = pos_pos[l]
+        l_pos_new = set()
+        m_pos = pos_pos[m]
+        m_pos_new = set()
+        for lp in l_pos:
+            for mp in m_pos:
+                if check_adjacent(lp, mp) is True:
+                    l_pos_new.add(lp)
+                    m_pos_new.add(mp)
+    else:
+        l_pos_new = set(pos_pos[l])
+        m_pos_new = set(pos_pos[m])
+    return l_pos_new, m_pos_new
+
+
+
+def unset_adjacent_to_letter(l):
+    if l in letters_set:
+        pass
+    else:
+        pos_pos = all_possible_remove_none()
+        l_pos_list = [unset_adjacent_pair(l, m, pos_pos)[0] for m in letters_adj[l]]
+        return set.intersection(*l_pos_list)
+
+def all_unset_adjacent_pairs_set_unique():
+    unset_queue = copy.deepcopy(letters_unset)
+    for l in unset_queue:
+        l_pos = unset_adjacent_to_letter(l)
+        if len(l_pos) == 1:
+            place_letter(l, list(l_pos)[0])
+
+
+def set_unique_and_unset_pairs():
+    while True:
+        if len(letters_unset) == 0:
+            return False
+        else:
+            set_unique_cycle()
+            all_unset_adjacent_pairs_set_unique()
+
+
+get_input(2)
+
+print_grid()
+
+set_unique_and_unset_pairs()
+
+print_grid()
--- gogen_bipartite.py
+++ gogen_bipartite.py
@@ -7,5 +7,4 @@ B = nx.Graph()
 B.add_nodes_from(string.ascii_uppercase[:25].split(), bipartite=0)
 B.add_nodes_from(range(25), bipartite=1)
 
-app = Viewer(B)
-app.mainloop()
+print B
